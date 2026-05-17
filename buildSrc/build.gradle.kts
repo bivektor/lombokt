5a49ea374c69faefa7ba55c7a29b373c1244f854
Çağatay Kalan <cagatay@bivektor.com>
2026-05-17 04:41:19 +0300

    Remove unused Dokka plugin from build configuration.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,9 +15,6 @@ dependencies {
   val deployerPluginVersion: String by project
   implementation("io.deepmedia.tools.deployer:io.deepmedia.tools.deployer.gradle.plugin:$deployerPluginVersion")
 
-  val dokkaVersion: String by project
-  implementation("org.jetbrains.dokka:dokka-gradle-plugin:$dokkaVersion")
-
   val binaryCompatibilityValidatorVersion: String by project
   implementation("org.jetbrains.kotlinx.binary-compatibility-validator:org.jetbrains.kotlinx.binary-compatibility-validator.gradle.plugin:$binaryCompatibilityValidatorVersion")
 
