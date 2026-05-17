5be8fbe75698b99182dcde9c8e1bd318a3a0ffc3
Çağatay Kalan <cagatay@bivektor.com>
2026-05-17 04:56:21 +0300

    Fix Maven example

--- examples/maven/src/test/kotlin/example/lombokt/CombinedAnnotationsTest.kt
+++ examples/maven/src/test/kotlin/example/lombokt/CombinedAnnotationsTest.kt
@@ -1,6 +1,5 @@
 package example.lombokt
 
-import lombokt.Buildable
 import lombokt.EqualsAndHashCode
 import lombokt.ToString
 import kotlin.test.Test
@@ -9,26 +8,18 @@ import kotlin.test.assertNotEquals
 
 class CombinedAnnotationsTest {
   @Suppress("unused")
-  @Buildable
   @ToString
   @EqualsAndHashCode
-  class Person(val name: String = "x", var surname: String = "y") {
-
-    @Buildable.Builder
-    class Builder {
-      fun name(name: String) = this
-      fun surname(surname: String) = this
-      fun build() = Person()
-    }
-  }
+  class Person(val name: String = "x", var surname: String = "y")
 
   @Test
   fun testAllAnnotations() {
-    val p1 = Person.Builder().build()
-    val p2 = Person.Builder().build()
+    val p1 = Person()
+    val p2 = Person()
     assertEquals("Person(name=x, surname=y)", p1.toString())
     assertEquals(p1, p2)
     assertEquals(p1.hashCode(), p2.hashCode())
-    assertNotEquals(p1, Person.Builder().name("a").build())
+    assertNotEquals(Person("a"), Person("b"))
+    assertNotEquals(Person(surname = "a"), Person(surname = "b"))
   }
 }
